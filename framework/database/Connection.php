3f309a71c1147396bd2dfbf88c0d63470cf926f8
m97 <mimounhaddou@iesebre.com>
2018-09-28 16:27:46 +0200

    Sync

--- framework/database/Connection.php
+++ framework/database/Connection.php
@@ -5,11 +5,11 @@ class Connection {
     {
 //        $config = App::resolve('config');
 //        var_dump($config);
-        $database = config('database');
-        var_dump($database);
+//        $database = config('database');
+//        var_dump($database);
         try {
             return new PDO('mysql:host=127.0.0.1;dbname=tasques','debian-sys-maint','znx3bZvss6oC0Mpp');
-            return new PDO("$database{'type'}:host=127.0.0.1;dbname=php_learning','debian-sys-maint','znx3bZvss6oC0Mpp");
+//            return new PDO("$database{'type'}:host=127.0.0.1;dbname=php_learning','debian-sys-maint','znx3bZvss6oC0Mpp");
 //            return new PDO('mysql:host=127.0.0.1;dbname=tasques','debian-sys-maint','znx3bZvss6oC0Mpp');
         } catch (\PDOException $e) {
             die('Could not connect: ' . $e);
